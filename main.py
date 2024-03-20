967636e6880ec2ef2b882b7629a32e6567132082
ReinisDelvers <141557986+ReinisDelvers@users.noreply.github.com>
2024-03-20 13:40:01 +0200

    .

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
-
-
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Sveiki!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(port = 5000)
-
-
-
-
-
-print("Sveiki")
-
-
-
-
-z = 4
-y = "asdg"
-if z == 4 :
-    print(y)
